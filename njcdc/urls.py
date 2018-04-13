95d95cedc0b0b7d4c5f7378404dacf61013195c3
lujonathanh <lujonathanh@gmail.com>
2018-04-13 13:46:59 -0400

    Redirect from main page done

--- njcdc/urls.py
+++ njcdc/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.urls import include, path
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('calc/', include('calc.urls')),
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='calc/'))
 ]
