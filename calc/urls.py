f5f44a7d4ee89abe714cd0bb00915ec59d993f30
lujonathanh <lujonathanh@gmail.com>
2018-04-16 21:33:29 -0400

    Database with ID being deleted

--- calc/urls.py
+++ calc/urls.py
@@ -5,8 +5,14 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('input/', views.input, name='input'),
+    # path('input/', views.updating_input, name='input'),
     path('plot/', views.plot, name='plot'),
-    path('results/', views.results, name='results'),
+
+
+    path('results/<int:id>', views.results, name='results'),
+    # path('results/', views.results, name='results'),
+
     path('about/', views.about, name='about'),
-    path('chart/', views.chart, name='chart')
+    path('chart/', views.chart, name='chart'),
+    path('ajax/load-gasoline-units/', views.load_gasoline_units, name='ajax_load_gasoline_units'),
 ]
--- calc/views.py
+++ calc/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.contrib import messages
 from django.urls import reverse
-from .forms import InputForm
+from .forms import InputForm, UpdatingInputForm
+from .models import get_possible_gasoline_units, UserProfile
 
 
 def index(request):
@@ -26,21 +27,22 @@ def about(request):
     return HttpResponse(template.render(context, request))
 
 def input(request):
-    # request.session["blah"] = "blarg"  # session test code from Changyan
-    # request.session.modified = True
-    # s = request.session
-    # m = s.modified
-    # i = s.items()
-    # meth = request.method
+
     if request.method == 'POST':
         input_form = InputForm(request.POST)
 
         if input_form.is_valid():
-            user = input_form.save(commit=False)
-            request.session["user"] = user
+            user = input_form.save()
+
+
+            # FOR SESSIONS, UNCOMMENT THE BELOW
+            # user = input_form.save(commit=False)
+            # request.session["user"] = user
+            # request.session.modified = True
             messages.success(request, "Thank you for entering")
-            request.session.modified = True
-            return HttpResponseRedirect(reverse('results'))
+
+
+            return HttpResponseRedirect(reverse('results', kwargs={'id': user.id}))
 
         messages.error(request, 'There were errors. Please try again.')
     else:
@@ -50,18 +52,49 @@ def input(request):
     context = {'input_form': input_form, 'formtitle': "Input"}
     return HttpResponse(template.render(context, request))
 
-def results(request):
-    # s = request.session  # session test code from Changyan
-    # m = s.modified
-    # i = s.items()
-    # up = s["user"]
-    up = request.session["user"]
+
+# def updating_input(request):
+#     if request.method == 'POST':
+#         input_form = UpdatingInputForm(request.POST)
+#
+#         if input_form.is_valid():
+#             user = input_form.save(commit=False)
+#             request.session["user"] = user
+#             messages.success(request, "Thank you for entering")
+#
+#             return HttpResponseRedirect(reverse('results'))
+#
+#         messages.error(request, 'There were errors. Please try again.')
+#     else:
+#         input_form = UpdatingInputForm()
+#
+#     template = loader.get_template('calc/input.html')
+#     context = {'input_form': input_form, 'formtitle': "Input"}
+#     return HttpResponse(template.render(context, request))
+
+def load_gasoline_units(request):
+    possible_units = get_possible_gasoline_units(request.GET.get('gasoline_type'))
+    template = loader.get_template('calc/gasoline_unit_dropdown.html')
+    context = {'units': possible_units}
+    return HttpResponse(template.render(context, request))
+
+
+# def results(request):
+#     up = request.session["user"]
+
+def results(request, id):
+    up = UserProfile.objects.get(id=id)
 
     up.calculate_net()
 
     template = loader.get_template('calc/results.html')
     context = {'up' : up}
-    return HttpResponse(template.render(context, request))
+
+    returner = HttpResponse(template.render(context, request))
+
+    UserProfile.objects.filter(id=id).delete()
+
+    return returner
 
 
     
