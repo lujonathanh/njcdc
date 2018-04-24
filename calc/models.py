64ef576f88ea739cb90557c0fa6b5b5891ff05b6
Agata Foryciarz <agata@foryciarz.com>
2018-04-23 20:59:43 -0400

    Update models.py

--- calc/models.py
+++ calc/models.py
@@ -1,4 +1,4 @@
-rom django.core.exceptions import ValidationError
+from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 from django.db import models
 
