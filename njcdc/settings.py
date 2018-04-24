4b5b102591ce5ea4543d4c6eb4b199ff14df8b7a
agataf <agata@foryciarz.com>
2018-04-23 20:56:49 -0400

    h

--- njcdc/settings.py
+++ njcdc/settings.py
@@ -11,21 +11,3 @@ DATABASES = {
     }
 }
 
-<<<<<<< HEAD
-=======
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
-django_heroku.settings(locals())
-
-SESSION_COOKIE_DOMAIN = "njcdc.herokuapp.com"
-
-# deployment stuff
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_SECONDS = 300
-SECURE_HSTS_PRELOAD = True
-DEBUG = False
->>>>>>> 2fe4cd33a253cf9908459a5f245fd9b100feda8a
